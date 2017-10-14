7cfdaf299a0e6b55717c7483553ec0bf134b9dbb
Matt Simons <matt@pi-top.com>
2017-10-14 14:52:17 +0100

    Create ticker.go

---
+++ ticker.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+        "fmt"
+        "github.com/d4l3k/talib"
+        "time"
+        "flag"
+)
+
+func main() {
+
+        strategy := *flag.String("strategy", "sma", "The trading strategy")
+        //interval := *flag.Int("interval", 5, "The refresh frequency")
+
+        flag.Parse()
+
+        markets := make([]*Market, 0)
+        for _, marketName := range(flag.Args()) {
+                markets = append(markets, NewMarket(marketName, 3 * time.Second))
+        }
+
+        fmt.Println("Using strategy: ", strategy, "At intervals: "/*, interval*/)
+
+        for {
+                for _, m := range(markets) {
+                        time.Sleep(5 * time.Second)
+                        sma := talib.Sma(m.History, HISTORY_LENGTH)[0]
+
+                        fmt.Println(m.Name)
+                        fmt.Println("SMA:  ", sma)
+                        fmt.Println("LAST: ", m.Last)
+
+                        if (m.Last > sma) {
+                                fmt.Println("BUY")
+                        } else {
+                                fmt.Println("SELL")
+                        }
+                        fmt.Println()
+                }
+        }
+}
